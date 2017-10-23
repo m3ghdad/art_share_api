8092de239922c530c282867816217c54d3153a8e
m3ghdad <example@email.com>
2017-10-23 15:37:04 -0700

    Adds artwork_shares_controller and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
 		if user.save
 		 render json: user
 		else 
-			render json: user.errors.full_messages, status: :unprocessable_entity		end
+			render json: user.errors.full_messages, status: :unprocessable_entity
+		end
 	end
 
 	def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :users, only: [:create, :destroy, :index, :show, :update]
-  resources :artworks, only: [:create, :destroy, :index, :show, :update]
+  resources :users, only: [:create, :destroy, :index, :show, :update] do
+    resources :artworks, only: [:index]
+  end
+
+  resources :artworks, only: [:create, :destroy, :show, :update]
+  resources :artwork_shares, only: [:create, :destroy]
+
 
   # get 'users', to: 'users#index', as: 'users'
   # get 'users/:id', to: 'users#show', as: 'user' 
