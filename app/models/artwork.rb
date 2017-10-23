1540a463d8fb3d9cccd81ccbbd5e530e78d0a5a8
m3ghdad <example@email.com>
2017-10-23 14:18:29 -0700

    Adds model for artwoekshare and associations for user and artwork

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -17,8 +17,13 @@ class Artwork < ApplicationRecord
 	# validates :artist_id, :title, uniqueness: {scope: :user}
 
 	belongs_to :artist, 
-		primary_id: :id, 
+		primary_key: :id, 
 		foreign_key: :artist_id,
 		class_name: 'User'
 
-end
+	has_many :artwork_shares,
+		primary_key: :id,
+		foreign_key: :artwork_id,
+		class_name: 'ArtworkShare'
+
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,12 @@ class User < ApplicationRecord
 	validates :username, presence: true, uniqueness: true
 
 	has_many :artworks,
-		primary_id: :id,
+		primary_key: :id,
 		foreign_key: :artist_id,
 		class_name: 'Artwork'
+
+	has_many :arwork_shares,
+		primary_key: :id,
+		foreign_key: :viewer_id,
+		class_name: 'ArtworkShare'
 end
