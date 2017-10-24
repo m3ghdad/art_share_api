4be98a7ff6f02dd2de852ef858d507a4b36510de
Meghdad Abbaszadegan <example@email.com>
2017-10-23 17:29:27 -0700

    Adds minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   end
 
   resources :artworks, only: [:create, :destroy, :show, :update]
-  resources :artwork_shares, only: [:create, :destroy]
   resources :comments, only: [:create, :destroy, :index]
+  resources :artwork_shares, only: [:create, :destroy]
+
 
   # get 'users', to: 'users#index', as: 'users'
   # get 'users/:id', to: 'users#show', as: 'user' 
